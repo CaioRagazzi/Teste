7819be9ef32e4cc7610dcbc9baa543d9af0e8d8e
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2018-10-10 10:58:13 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,6 +5,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building 123..'
+                bat "dotnet build --configuration Release"
             }
         }
         stage('Test') {
